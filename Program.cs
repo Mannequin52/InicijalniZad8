52bded9064dd6f7b95fa53fe39950ef5069e5947
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-15 17:09:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad8
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            /*Fibonaccijev niz definiran je na sljedeći način:
+    • prvi član niza je 0
+    • drugi član niza je 1
+    • svaki sljedeći član niza jednak je zbroju prethodna dva člana
+    Napišite program koji će zatražiti od korisnika da unese broj n, te će izračunati n-ti član Fibonaccijevog niza.
+    */
+            Console.WriteLine("Unesi neki broj: ");
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            int prvi = 0, drugi = 1;
+            int zbroj = drugi + prvi;
+            for (int i = 0; i < b; i++)
+            {
+                drugi = drugi + zbroj;
+            }
+            Console.WriteLine(drugi);
+            Console.ReadKey();
+
+        }
+    }
+}
